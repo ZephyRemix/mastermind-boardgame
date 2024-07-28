cbc19737b04c6fd6be6a07001eb7da798c8d450f
ZephyRemix <hunyaochong@gmail.com>
2024-07-28 15:39:23 +0800

    Ready

--- code_breaker.rb
+++ code_breaker.rb
@@ -3,10 +3,6 @@ require_relative 'code_peg'
 
 # The player with this role will be tasked to solve the mystery code
 class CodeBreaker < RoleFactory
-  # def initialize
-  #   super
-  # end
-
   def set_pegs(color_code, code_peg_ansi, _code_breaker)
     self.move = color_code.map { |code| CodePeg.new(code_peg_ansi[code]) }
   end
